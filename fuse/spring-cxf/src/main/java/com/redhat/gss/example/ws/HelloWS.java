e6c3fbcc2cd843cf6950f8118d98a3fdad976021
Jason Shepherd <jshepherd@redhat.com>
2013-08-30 16:36:22 +1000

    added spring-cxf example

---
+++ fuse/spring-cxf/src/main/java/com/redhat/gss/example/ws/HelloWS.java
@@ -0,0 +1,22 @@
+/*
+ * To the extent possible under law, Red Hat, Inc. has dedicated all copyright 
+ * to this software to the public domain worldwide, pursuant to the CC0 Public 
+ * Domain Dedication. This software is distributed without any warranty.  See 
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+package com.redhat.gss.example.ws;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWS
+{
+public static final Logger logger = LoggerFactory.getLogger(HelloWS.class);
+
+  public String hello(String name) throws Exception
+  {
+	logger.info("Hello called on HelloWS with name: " + name);
+    return "Hello, " + name;
+  }
+}
