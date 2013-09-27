56407dcd2fce195c29f66067b3ea67c77ddccf04
Jason Shepherd <jshepherd@redhat.com>
2013-09-27 15:42:28 +1000

    split ws-gen

--- fuse/spring-cxf-httpprop/src/main/java/com/fusesource/demo/wsdl/customerservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://demo.fusesource.com/wsdl/CustomerService/")
-package com.fusesource.demo.wsdl.customerservice;
--- fuse/spring-cxf-httpprop/ws-server/src/main/java/com/fusesource/demo/customer/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.fusesource.com/demo/customer", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.fusesource.demo.customer;
