4e590cd941bd8efaf0060d108d17ff39f7352efc
Jason Shepherd <jshepherd@redhat.com>
2013-09-27 15:31:22 +1000

    split parent pom

---
+++ fuse/spring-cxf-httpprop/ws-server/src/main/java/com/fusesource/demo/customer/ObjectFactory.java
@@ -0,0 +1,53 @@
+
+package com.fusesource.demo.customer;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the com.fusesource.demo.customer package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _Customer_QNAME = new QName("http://www.fusesource.com/demo/customer", "Customer");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: com.fusesource.demo.customer
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link Customer }
+     * 
+     */
+    public Customer createCustomer() {
+        return new Customer();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link Customer }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://www.fusesource.com/demo/customer", name = "Customer")
+    public JAXBElement<Customer> createCustomer(Customer value) {
+        return new JAXBElement<Customer>(_Customer_QNAME, Customer.class, null, value);
+    }
+
+}
---
+++ fuse/spring-cxf-httpprop/ws-server/src/main/java/com/fusesource/demo/customer/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.fusesource.com/demo/customer", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.fusesource.demo.customer;
